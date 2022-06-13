72b12aea5aefbe073f24b0b4b64522383657df27
WDamaris <damarisshish@gmail.com>
2022-06-13 16:19:27 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(array , name){
+    return array.filter((match) => match.toLowerCase() === name.toLowerCase())
+}
+const fuzzyMatch = (array,names)=>{
+    let newArray = []
+    for (const name of array){
+        if ( name.slice(0,1) === names.slice(0,1)){
+            newArray.push(name)
+        }
+    }
+    return newArray
+}
+const matchName = (array,structure)=>{
+    const newDataArray=[]
+    for (const dataStruct of array){
+        if (dataStruct.name.toLowerCase()=== structure.toLowerCase()||
+        dataStruct.hometown.toLowerCase()=== structure.toLowerCase()){
+            newDataArray.push(dataStruct)
+        }
+    }
+    return newDataArray
+}
